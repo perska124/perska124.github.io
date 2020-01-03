ab349d10989c3b854bcc7da06ed2b80b0d300658
perska124 <perska124@wp.pl>
2020-01-03 01:11:03 +0100

    Show active paragraph in menu

---
+++ main-script.js
@@ -0,0 +1,17 @@
+window.onscroll = function() {
+    var scrollDistance = window.scrollY;
+
+    for(paragraph of document.getElementsByTagName("p")){
+        if(paragraph.offsetTop <= scrollDistance){
+            const menuItems = document.getElementsByClassName('menu-item');
+            for(menuItem of menuItems) {
+                menuItem.classList.remove("active");
+                const href = menuItem.getAttribute("href");
+                if(href.slice(1,href.length)===paragraph.id){
+                    menuItem.classList.add('active');
+                }
+            }          
+        }
+    }
+    
+};
